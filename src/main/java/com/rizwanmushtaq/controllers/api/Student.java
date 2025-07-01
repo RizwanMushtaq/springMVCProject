c062bede9e1d5d8939c3093f4426f9ac4630bbb0
Rizwan <rizwanmushtaq15@gmail.com>
2025-07-01 11:18:50 +0200

    updated student endpoints

--- src/main/java/com/rizwanmushtaq/controllers/api/Student.java
+++ src/main/java/com/rizwanmushtaq/controllers/api/Student.java
@@ -2,13 +2,14 @@ package com.rizwanmushtaq.controllers.api;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController("/api")
+@RestController
+@RequestMapping("/api")
 public class Student {
   @GetMapping("/students")
   public List<Student> getAllStudents() {
@@ -19,8 +20,8 @@ public class Student {
   }
 
   @PostMapping("/student")
-  public Student createStudent(@RequestBody Student student) {
+  public String createStudent(Student student) {
     // Logic to save the new student
-    return student;
+    return "Student created successfully: " + student.toString();
   }
 }
